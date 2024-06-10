cd6cfc3abaaaa3c36dd2a2e24bc7470438451f9e
Srin <60167747+SrinSS01@users.noreply.github.com>
2024-06-10 12:42:45 +0000

    no message

--- api/get/Schedules.go
+++ api/get/Schedules.go
@@ -1,1 +1,48 @@
 package get
+
+import (
+	"net/http"
+	"s2wavy/selfbot/api/types"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase"
+)
+
+type ScheduleRequest struct {
+	Path string
+	App  *pocketbase.PocketBase
+}
+
+func (d *ScheduleRequest) Execute(c echo.Context) error {
+	guild_id := c.PathParam("guild_id")
+	selfbot_user_id := c.PathParam("selfbot_user_id")
+	channel_id := c.PathParam("channel_id")
+	var schedules []*types.MessageScheduling
+	if err := d.App.Dao().DB().
+		Select("*").
+		From("message_schedulings").
+		Where(dbx.NewExp("guild_id = {:guild_id} and selfbot_user_id = {:selfbot_user_id} and channel_id = {:channel_id}", dbx.Params{
+			"guild_id":        guild_id,
+			"selfbot_user_id": selfbot_user_id,
+			"channel_id":      channel_id,
+		})).
+		All(&schedules); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": err.Error(),
+			"error":   err,
+		})
+	}
+	for _, schedule := range schedules {
+		scheduledTime, _ := strconv.Atoi(schedule.InitiateTime)
+		schedule.Expired = time.Now().After(time.UnixMilli(int64(scheduledTime)))
+	}
+
+	return c.JSON(http.StatusOK, schedules)
+}
+
+var ScheduleRequestFunction = ScheduleRequest{
+	Path: "/schedules/:selfbot_user_id/:guild_id/:channel_id",
+}
--- api/post/Schedule.go
+++ api/post/Schedule.go
@@ -2,10 +2,14 @@ package post
 
 import (
 	"encoding/json"
-	"github.com/labstack/echo/v5"
-	"github.com/pocketbase/pocketbase"
 	"net/http"
 	"s2wavy/selfbot/api/types"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase"
 )
 
 type ScheduleRequest struct {
@@ -21,8 +25,30 @@ func (d *ScheduleRequest) Execute(c echo.Context) error {
 			"error":   err,
 		})
 	}
+	scheduledTime, _ := strconv.Atoi(schedule.InitiateTime)
+	if time.Now().After(time.UnixMilli(int64(scheduledTime))) {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "Please enter a valid time",
+		})
+	}
+
+	if _, err := d.App.Dao().DB().Insert("message_schedulings", dbx.Params{
+		"guild_id":        schedule.GuildID,
+		"channel_id":      schedule.ChannelID,
+		"selfbot_user_id": schedule.SelfbotUserID,
+		"message_content": schedule.MessageContent,
+		"initiate_time":   schedule.InitiateTime,
+		"interval":        schedule.Interval,
+		"expired":         schedule.Expired,
+	}).Execute(); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": err.Error(),
+			"error":   err,
+		})
+	}
+
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": schedule,
+		"message": "Scheduled",
 	})
 }
 
