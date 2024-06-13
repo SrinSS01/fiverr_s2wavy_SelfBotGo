f367e0ef749ccb1f082521389431aad2ce517d48
Srin <60167747+SrinSS01@users.noreply.github.com>
2024-06-13 03:48:15 +0000

    changed command usage from all users to only bot user

--- bots/SelfBot.go
+++ bots/SelfBot.go
@@ -1,10 +1,7 @@
 package bots
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/go-resty/resty/v2"
-	"net/http"
 	"regexp"
 	"s2wavy/selfbot/commands"
 	"time"
@@ -19,21 +16,28 @@ type SelfBot struct {
 }
 
 var (
+	// To add a new command just put the relevant name and execute function values as shown below
 	CommandHandlers = map[string]func(*discordgo.Session, *discordgo.Message, string){
 		commands.Ping.Command.Name: commands.Ping.Execute,
 	}
+
+
 	Bots         = map[string]*SelfBot{}
-	commandRegex = regexp.MustCompile("-(?P<name>[\\w-]+)(?:\\s+(?P<args>.+))?")
+	commandRegex = regexp.MustCompile(`-(?P<name>[\w-]+)(?:\s+(?P<args>.+))?`)
 )
 
 func ReadyEvent(s *discordgo.Session, r *discordgo.Ready) {
 	fmt.Println(r.User.Username, "is ready")
 }
 
-func MessageCreateEvent(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
+func MessageCreateEvent(session *discordgo.Session, event *discordgo.MessageCreate) {
+	if event.Author.ID != session.State.User.ID {
 		return
 	}
+	/* 
+	The commented out section is used to fetch message content of other users, by default message contents are not available 
+	for self-bots in the default message struct supplied by Message create event.
+	
 	request := resty.New().R()
 	request.SetHeader("Authorization", s.Token)
 	response, err := request.Execute("GET", "https://discord.com/api/v9/channels/"+m.ChannelID+"/messages?limit=1")
@@ -50,8 +54,9 @@ func MessageCreateEvent(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if err != nil {
 		fmt.Println(err)
 		return
-	}
-	matches := commandRegex.FindStringSubmatch(message[0].Content)
+	} */
+	message := event.Message
+	matches := commandRegex.FindStringSubmatch(message.Content)
 	if len(matches) == 0 {
 		return
 	}
@@ -61,5 +66,5 @@ func MessageCreateEvent(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if f == nil {
 		return
 	}
-	f(s, message[0], args)
+	f(session, message, args)
 }
